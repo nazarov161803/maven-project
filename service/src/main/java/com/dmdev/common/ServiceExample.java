62fef2689a87c371ff9b981a7938cec053f36e0a
nazar <n.nazarov@cloudaware.com>
2025-02-02 16:55:22 +0100

    Homework 01

---
+++ service/src/main/java/com/dmdev/common/ServiceExample.java
@@ -0,0 +1,9 @@
+package com.dmdev.common;
+
+import java.util.List;
+
+public class ServiceExample {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
