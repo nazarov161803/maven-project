96819eccd3f53001a911800e9d57990c8c04bbfd
nazar <n.nazarov@cloudaware.com>
2025-02-02 16:55:22 +0100

    HW01: homework added + checkstyle

---
+++ common/src/main/java/com/dmdev/common/CommonExample.java
@@ -0,0 +1,8 @@
+package com.dmdev.common;
+
+public class CommonExample {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
---
+++ common/src/test/java/com/dmdev/common/integration/CommonExampleIT.java
@@ -0,0 +1,12 @@
+package com.dmdev.common.integration;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CommonExampleIT {
+
+    @Test
+    void shouldReturnTrue() {
+        Assertions.assertTrue(true);
+    }
+}
